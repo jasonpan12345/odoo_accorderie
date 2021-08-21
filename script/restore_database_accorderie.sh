b5df9d34f98336f529f75544827d0d0ec98da3b5
Mathieu Benoit <mathben@technolibre.ca>
2021-08-20 21:56:27 -0400

    [FIX] truncate_fill database script noaccorderie, add logo

--- script/restore_database_accorderie.sh
+++ script/restore_database_accorderie.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SQL_PATH=/accorderie_canada/Intranet/accorder_AccorderieIntranet_20200826.sql
+
 echo "Create database and user"
 sudo mysql -u root << EOF
 DROP DATABASE IF EXISTS accorderie_log_2019;
@@ -10,10 +12,10 @@ CREATE DATABASE accorderie_log_2019;
 EOF
 
 echo "Fix SQL file"
-sed -i "s/'0000-00-00'/NULL/g" /accorderie_canada/Intranet/accorder_AccorderieIntranet_20200826.sql
+sed -i "s/'0000-00-00'/NULL/g" ${SQL_PATH}
 
 echo "Import SQL file"
-mysql -u accorderie -paccorderie accorderie_log_2019 < /accorderie_canada/Intranet/accorder_AccorderieIntranet_20200826.sql
+mysql -u accorderie -paccorderie accorderie_log_2019 < ${SQL_PATH}
 
 echo "Fix SQL in database"
 ./.venv/bin/python ./addons/TechnoLibre_odoo_accorderie/script/fix_database.py
