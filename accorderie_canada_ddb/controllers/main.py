928c2b94bfb29edf62e4ebd88a7afb44b2b9308e
Mathieu Benoit <mathben@technolibre.ca>
2022-01-30 00:10:54 -0500

    [IMP] accorderie_canada_ddb: snippet offre de services

--- accorderie_canada_ddb/controllers/main.py
+++ accorderie_canada_ddb/controllers/main.py
@@ -5,29 +5,73 @@ import werkzeug
 
 from odoo import http
 from odoo.http import request
+from datetime import datetime
+import humanize
 
 _logger = logging.getLogger(__name__)
 
 
 class AccorderieCanadaDdbController(http.Controller):
     @http.route(
-        ["/accorderie_canada_ddb/helloworld"],
+        [
+            "/offre/<int:offre_id>",
+        ],
+        type="http",
+        auth="public",
+        website=True,
+    )
+    def offre(self, offre_id=None, **kwargs):
+        env = request.env(context=dict(request.env.context))
+
+        Offre = env["accorderie.offre.service"]
+        if offre_id:
+            offre_ids = offre_id
+            offres = Offre.sudo().browse(offre_ids).exists()
+        else:
+            offres = None
+
+        dct_value = {"offre": offres}
+
+        # Render page
+        return request.render(
+            "accorderie_canada_ddb.offre_service_unit", dct_value
+        )
+
+    @http.route(
+        [
+            "/offre_list",
+        ],
         type="json",
         auth="public",
         website=True,
-        methods=["POST", "GET"],
-        csrf=False,
     )
-    def hello_world(self):
-        data_id = http.request.env["accorderie.echange.service"].search([])
-        dct_value = {}
-        if data_id:
-            dct_value["commentaire"] = data_id.commentaire
-            dct_value["date_echange"] = data_id.date_echange
-            dct_value["nb_heure"] = data_id.nb_heure
-            dct_value["nom_complet"] = data_id.nom_complet
-            dct_value["remarque"] = data_id.remarque
-        return dct_value
+    def offre_list(self, **kwargs):
+        env = request.env(context=dict(request.env.context))
+
+        Offre = env["accorderie.offre.service"]
+        offre_ids = Offre.search([], order="create_date desc", limit=3).ids
+        offres = Offre.sudo().browse(offre_ids)
+
+        lst_time_diff = []
+        timedate_now = datetime.now()
+        # fr_CA not exist
+        # check .venv/lib/python3.7/site-packages/humanize/locale/
+        _t = humanize.i18n.activate("fr_FR")
+        for offre in offres:
+            if offre.date_mise_a_jour:
+                diff_time = timedate_now - offre.date_mise_a_jour
+            else:
+                diff_time = timedate_now - offre.create_date
+            str_diff_time = humanize.naturaltime(diff_time).capitalize() + "."
+            lst_time_diff.append(str_diff_time)
+        humanize.i18n.deactivate()
+
+        dct_value = {"offres": offres, "lst_time": lst_time_diff}
+
+        # Render page
+        return request.env["ir.ui.view"].render_template(
+            "accorderie_canada_ddb.accorderie_liste_service_offert", dct_value
+        )
 
     @http.route(
         "/new/accorderie_accorderie", type="http", auth="user", website=True
--- accorderie_canada_ddb/static/src/js/website.accorderie_canada_ddb.animation.js
+++ accorderie_canada_ddb/static/src/js/website.accorderie_canada_ddb.animation.js
@@ -8,8 +8,10 @@ odoo.define("accorderie_canada_ddb.animation", function (require) {
 
         start: function () {
             var self = this;
+            var $eventList = this.$('.container');
+            this._originalContent = $eventList[0].outerHTML;
             var def = this._rpc({
-                route: "/accorderie_canada_ddb/helloworld",
+                route: "/offre_list",
             }).then(function (data) {
                 if (data.error) {
                     return;
@@ -19,48 +21,17 @@ odoo.define("accorderie_canada_ddb.animation", function (require) {
                     return;
                 }
 
-                if (data["commentaire"]) {
-                    self.$(".commentaire_value").text(data["commentaire"]);
-                }
-                if (data["date_echange"]) {
-                    self.$(".date_echange_value").text(data["date_echange"]);
-                }
-                if (data["demande_service"]) {
-                    self.$(".demande_service_value").text(
-                        data["demande_service"]
-                    );
-                }
-                if (data["membre_acheteur"]) {
-                    self.$(".membre_acheteur_value").text(
-                        data["membre_acheteur"]
-                    );
-                }
-                if (data["membre_vendeur"]) {
-                    self.$(".membre_vendeur_value").text(
-                        data["membre_vendeur"]
-                    );
-                }
-                if (data["nb_heure"]) {
-                    self.$(".nb_heure_value").text(data["nb_heure"]);
-                }
-                if (data["nom_complet"]) {
-                    self.$(".nom_complet_value").text(data["nom_complet"]);
-                }
-                if (data["offre_service"]) {
-                    self.$(".offre_service_value").text(data["offre_service"]);
-                }
-                if (data["point_service"]) {
-                    self.$(".point_service_value").text(data["point_service"]);
-                }
-                if (data["remarque"]) {
-                    self.$(".remarque_value").text(data["remarque"]);
-                }
-                if (data["type_echange"]) {
-                    self.$(".type_echange_value").text(data["type_echange"]);
-                }
+                self._$loadedContent = $(data);
+                $eventList.replaceWith(self._$loadedContent);
             });
 
             return $.when(this._super.apply(this, arguments), def);
+        },
+        destroy: function () {
+            this._super.apply(this, arguments);
+            if (this._$loadedContent) {
+                this._$loadedContent.replaceWith(this._originalContent);
+            }
         },
     });
 });
