180c228cd62b3df1d873158f50c12a93501d87e3
Mathieu Benoit <mathben@technolibre.ca>
2022-04-26 04:13:04 -0400

    [ADD] accorderie_canada_ddb_website

---
+++ accorderie_canada_ddb_website/hooks.py
@@ -0,0 +1,19 @@
+import os
+
+from odoo import SUPERUSER_ID, _, api, fields, models
+
+
+def pre_init_hook(cr):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        # Remove all website pages before installing data
+
+        website_page_ids = env["website.page"].search([])
+        website_menu_ids = env["website.menu"].search([])
+        # TODO website doesn't support multi
+        # website_page_ids.website_id = None
+        # TODO replace by :
+        for website_page in website_page_ids:
+            website_page.website_id = None
+        for website_menu in website_menu_ids:
+            website_menu.website_id = None
