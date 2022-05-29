24869815a4b90a30d1144c3a9fae25b22bbb88c7
Mathieu Benoit <mathben@technolibre.ca>
2022-05-29 04:29:28 -0400

    [ADD] demo_accorderie_canada_ddb

---
+++ demo_accorderie_canada_ddb/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Demo Accorderie Canada",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "website": "https://technolibre.ca",
+    "depends": ["accorderie_canada_ddb"],
+    "data": [
+        "data/accorderie_accorderie.xml",
+        "data/accorderie_membre.xml",
+        "data/accorderie_offre_service.xml",
+        "data/accorderie_demande_service.xml",
+    ],
+    "installable": True,
+}
