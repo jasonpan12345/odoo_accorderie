50c370cb7e18f8770b71d83c548b4e017adac20f
Jason Pan <jason.pan@technolibre.ca>
2022-06-07 17:07:27 -0400

    [UPD] theme_accorderie_snippet: Debug options (highlight/hide)

--- accorderie_canada_ddb/static/src/js/website.accorderie_canada_ddb.debug_not_implemented.js
+++ accorderie_canada_ddb/static/src/js/website.accorderie_canada_ddb.debug_not_implemented.js
@@ -4,7 +4,7 @@ odoo.define('website.accorderie_canada_ddb.debug_not_implemented.instance', func
     require('web_editor.ready');
     let Debug = require('website.accorderie_canada_ddb.debug_not_implemented');
 
-    let $debug = $('.debug_not_implemented');
+    let $debug = $('.highlight_not_implemented');
     if (!$debug.length) {
         return null;
     }
@@ -25,18 +25,32 @@ odoo.define("website.accorderie_canada_ddb.debug_not_implemented", function (req
     let Debug = Widget.extend({
         start: function () {
             let self = this;
-            let debugHidden = true;
+            let debugHighlighted = false;
+            let debugHide = false;
             this._super.apply(this.arguments).then(function () {
-                $('.debug_not_implemented')
-                    .off('click')
+                $('.highlight_not_implemented')
                     .click(function (ev) {
                         self.on_click(ev);
-                        if(debugHidden) {
-                            debugHidden = false;
-                            self.ToggleNotImplemented(true);
+                        if(debugHighlighted) {
+                            debugHighlighted = false;
+                            self.HighlightNotImplemented(false);
                         } else {
-                            debugHidden = true;
-                            self.ToggleNotImplemented(false)
+                            debugHighlighted = true;
+                            self.HighlightNotImplemented(true)
+                        }
+
+                    });
+            });
+            this._super.apply(this.arguments).then(function () {
+                $('.hide_not_implemented')
+                    .click(function (ev) {
+                        self.on_click(ev);
+                        if(debugHide) {
+                            debugHide = false;
+                            self.HideNotImplemented(false);
+                        } else {
+                            debugHide = true;
+                            self.HideNotImplemented(true)
                         }
 
                     });
@@ -47,24 +61,31 @@ odoo.define("website.accorderie_canada_ddb.debug_not_implemented", function (req
             ev.stopPropagation();
         },
 
-        ToggleNotImplemented: function (show) {
+        HighlightNotImplemented: function (show) {
+            console.log("debug");
 
-            let unimplementedClasses = ["s_actualite", "s_event_list", "s_activite_bilan", "s_mes_informations",
-                "s_vertical_list_lg", "s_pub_offre_desc", "activites_messages", "mes_accordages"];
+            let classes = document.getElementsByClassName("debug_unimplemented");
 
-            for (let i = 0; i < unimplementedClasses.length; i++) {
-                let classes = document.getElementsByClassName(unimplementedClasses[i]);
-
-                for (let j = 0; j < classes.length; j++) {
-                    if(show) {
-                        classes[j].style.backgroundColor = "rgba(255,0,0,0.5)";
-                    } else {
-                        classes[j].style.backgroundColor = "unset";
-                    }
-
+            for (let j = 0; j < classes.length; j++) {
+                if (show) {
+                    classes[j].style.backgroundColor = "rgba(255,0,0,0.5)";
+                } else {
+                    classes[j].style.backgroundColor = "unset";
                 }
             }
+        },
+        HideNotImplemented: function (show) {
+            console.log("debug");
 
+            let classes = document.getElementsByClassName("debug_unimplemented");
+
+            for (let j = 0; j < classes.length; j++) {
+                if (show) {
+                    classes[j].style.display = "none";
+                } else {
+                    classes[j].style.display = "block";
+                }
+            }
         },
     });
 
